00ab83bab94a61a3c58a1afe28d77a828d149d72
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-25 18:36:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
 
+function increaseRankBy(n) {
+  const lis=document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = parseInt(lis[i].innerHTML,10)+n
+}
+}
+
+function deepestChild() {
+  return document.querySelector('div.grand-node div div div')
+}
